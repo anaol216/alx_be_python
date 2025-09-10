77f282e7bc1876748cedf8884a0c360116b01690
anaol216 <atinafuanaol@gmail.com>
2025-09-10 12:06:05 +0300

    Add simple interest calculation script

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,6 @@
+# calculating company simple interest
+principal = 1000  # principal amount
+rate = 0.05  # interest rate
+time = 3  # time in years
+interest = principal * rate * time  # simple interest formula
+print("Simple interest is:", interest)
